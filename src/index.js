c701b88f13bb5d8f3f566f2408ea042b8a8a57ee
George Campbell <campbell.goe@gmail.com>
2020-06-02 18:33:25 +0100

    emit 'model-load'progress' event when loading gltf model

--- src/index.js
+++ src/index.js
@@ -89,7 +89,7 @@ require('./core/a-mixin');
 require('./extras/components/');
 require('./extras/primitives/');
 
-console.log('A-Frame Version: 1.0.4 (Date 2020-04-15, Commit #441c3f2b)');
+console.log('A-Frame Version: 1.0.4 (Date 2020-06-02, Commit #ec3b2b63)');
 console.log('THREE Version (https://github.com/supermedium/three.js):',
             pkg.dependencies['super-three']);
 console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
